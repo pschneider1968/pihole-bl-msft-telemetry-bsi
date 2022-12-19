56810eae15cc80a6acad9b85b4ef4f514248d87b
pschneider1968 <36973164+pschneider1968@users.noreply.github.com>
2022-12-19 23:22:30 +0100

    Nuke lists before reloading them from files, and restart DNS service finally

--- import_lists.sh
+++ import_lists.sh
@@ -6,22 +6,28 @@ ERROR=1
 PIHOLE_CMD=$(which pihole)
 
 WE_FILE=whitelist_exact.txt
-WE_FLAGS='-w'
+WE_FLAGS='-w -nr'
 
 WR_FILE=whitelist_regex.txt
-WR_FLAGS='--white-regex'
+WR_FLAGS='--white-regex -nr'
 
 BE_FILE=blacklist_exact.txt
-BE_FLAGS='-b'
+BE_FLAGS='-b -nr'
 
 BR_FILE=blacklist_regex.txt
-BR_FLAGS='--regex'
+BR_FLAGS='--regex -nr'
+
+NUKE_FLAGS='--nuke'
 
 UPDATE_DB_FLAGS='-g'
+RELOAD_FLAGS='restartdns reloadlists'
 
 process_file () {
 
 if [ -f $1 ]; then
+
+    echo Nuking list $2...
+    $PIHOLE_CMD $2 $NUKE_FLAGS
 
     cat $1 | grep -v '#' | grep -v -e '^$' | sort | while read LINE
     do
@@ -66,7 +72,11 @@ process_file $BR_FILE $BR_FLAGS
 echo
 echo Now updating all adlists and Gravity DB...
 $PIHOLE_CMD $UPDATE_DB_FLAGS
+
 echo
+echo Restarting DNS service...
+$PIHOLE_CMD $RELOAD_FLAGS
+
 echo Done!
 
 exit $SUCCESS
