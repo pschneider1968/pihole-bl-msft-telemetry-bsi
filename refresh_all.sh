b10f2ec7c40973d4cc4977b4c741036ee8069f41
pschneider1968 <36973164+pschneider1968@users.noreply.github.com>
2022-12-20 00:18:29 +0100

    Improve output of refresh_all.sh a bit

--- refresh_all.sh
+++ refresh_all.sh
@@ -5,8 +5,14 @@ REFRESH_CMD="git pull --no-rebase"
 LOG_FILE="/var/log/pihole/refresh_pihole_lists.log"
 REFRESH_SCRIPT="import_lists.sh"
 
-cd $DIRECTORY
-$REFRESH_CMD
-
 touch $LOG_FILE
+
+cd $DIRECTORY
+$REFRESH_CMD >> $LOG_FILE
+
+echo --------------------------------------- >> $LOG_FILE
+echo Refreshing lists at $(date) >> $LOG_FILE
+echo ---------------------------------------  >> $LOG_FILE
+echo  >> $LOG_FILE
+
 $SHELL $REFRESH_SCRIPT | tee $LOG_FILE | cat
