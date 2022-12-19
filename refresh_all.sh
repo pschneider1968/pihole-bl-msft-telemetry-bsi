92cfeec8bdd74834ac065f026c6c44d4ad0454ba
Peter Schneider <36973164+pschneider1968@users.noreply.github.com>
2022-12-20 00:35:21 +0100

    Update refresh_all.sh

--- refresh_all.sh
+++ refresh_all.sh
@@ -7,15 +7,10 @@ REFRESH_SCRIPT="import_lists.sh"
 
 touch $LOG_FILE
 
-<<<<<<< HEAD
 echo -------------------------------------------------- | tee -a $LOG_FILE | cat
 echo Refreshing lists at $(date)                        | tee -a $LOG_FILE | cat
 echo -------------------------------------------------- | tee -a $LOG_FILE | cat
-=======
-echo --------------------------------------- | tee -a $LOG_FILE | cat
-echo Refreshing lists at $(date)             | tee -a $LOG_FILE | cat
-echo --------------------------------------- | tee -a $LOG_FILE | cat
->>>>>>> ee9929051bf4c71f08e551b527eadf9e752dab73
+
 echo | tee -a $LOG_FILE | cat
 
 cd $DIRECTORY
@@ -23,13 +18,8 @@ $REFRESH_CMD | tee -a $LOG_FILE | cat
 
 $SHELL $REFRESH_SCRIPT | tee $LOG_FILE | cat
 
-<<<<<<< HEAD
 echo ------------------------------------------------- | tee -a $LOG_FILE | cat
 echo Done refreshing lists at $(date)                  | tee -a $LOG_FILE | cat
 echo ------------------------------------------------- | tee -a $LOG_FILE | cat
-=======
-echo --------------------------------------- | tee -a $LOG_FILE | cat
-echo Done refreshing lists at $(date)        | tee -a $LOG_FILE | cat
-echo --------------------------------------- | tee -a $LOG_FILE | cat
->>>>>>> ee9929051bf4c71f08e551b527eadf9e752dab73
+
 echo | tee -a $LOG_FILE | cat
