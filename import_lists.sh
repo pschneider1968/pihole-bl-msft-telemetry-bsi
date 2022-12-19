620123a044e687b7f26be0616ddd6f29d80c3bea
pschneider1968 <36973164+pschneider1968@users.noreply.github.com>
2022-12-19 22:07:25 +0100

    - changed whitelist_regext.txt to only contain the patterns - added import script

---
+++ import_lists.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+SUCCESS=0
+ERROR=1
+
+PIHOLE_CMD=$(which pihole)
+
+WE_FILE=whitelist_exact.txt
+WE_FLAGS='-w'
+
+WR_FILE=whitelist_regex.txt
+WR_FLAGS='--white-regex'
+
+BE_FILE=blacklist_exact.txt
+BE_FLAGS='-b'
+
+BR_FILE=blacklist_regex.txt
+BR_FLAGS='--regex'
+
+UPDATE_DB_FLAGS='-g'
+
+process_file () {
+
+if [ -f $1 ]; then
+
+    cat $1 | grep -v '#' | grep -v -e '^$' | sort | while read LINE
+    do
+        echo $PIHOLE_CMD $2 $LINE
+    done
+
+else
+    echo File $1 not found!
+fi
+
+}
+
+
+if [ ! -z $PIHOLE_CMD ]; then
+    if [ -x $PIHOLE_CMD ]; then
+        echo Found executable Pi-Hole command $PIHOLE_CMD!
+    else
+        echo Pi-Hole command $PIHOME_CMD not found, aborting!
+        exit $ERROR
+    fi
+else
+    echo Pi-Hole command $PIHOME_CMD not found, aborting!
+    exit $ERROR
+fi
+
+echo
+echo Processing $WE_FILE...
+process_file $WE_FILE $WE_FLAGS
+
+echo
+echo Processing $WR_FILE...
+process_file $WR_FILE $WR_FLAGS
+
+echo
+echo Processing $BE_FILE...
+process_file $BE_FILE $BE_FLAGS
+
+echo
+echo Processing $BR_FILE...
+process_file $BR_FILE $BR_FLAGS
+
+echo
+echo Now updating all adlists and Gravity DB...
+echo $PIHOLE_CMD $UPDATE_DB_FLAGS
+echo
+echo Done!
+
+exit $SUCCESS
