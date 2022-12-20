7327d8535132dfe231d18b211dc54f01eaef6803
pschneider1968 <36973164+pschneider1968@users.noreply.github.com>
2022-12-20 16:21:14 +0100

    Add git status

--- refresh_all.sh
+++ refresh_all.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 DIRECTORY="/etc/pihole/pihole-bl-msft-telemetry-bsi"
+GIT_STATUS_CMD="git status"
 REFRESH_CMD="git pull --no-rebase"
 LOG_FILE="/var/log/pihole/refresh_pihole_lists.log"
 REFRESH_SCRIPT="import_lists.sh"
@@ -14,7 +15,11 @@ echo -------------------------------------------------- | tee -a $LOG_FILE | cat
 echo | tee -a $LOG_FILE | cat
 
 cd $DIRECTORY
+$GIT_STATUS_CMD | tee -a $LOG_FILE | cat
+
+echo Pulling from remote repo... | tee -a $LOG_FILE | cat
 $REFRESH_CMD | tee -a $LOG_FILE | cat
+$GIT_STATUS_CMD | tee -a $LOG_FILE | cat
 
 $SHELL $REFRESH_SCRIPT | tee -a $LOG_FILE | cat
 
