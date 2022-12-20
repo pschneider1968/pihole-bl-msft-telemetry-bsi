eaf3548dd93fecba587ac622b00e615582b8e976
pschneider1968 <36973164+pschneider1968@users.noreply.github.com>
2022-12-20 22:19:52 +0100

    Blocklist file list_of_blocklists.txt is now also imported into pihole Gravity.db via sqlite3

--- import_lists.sh
+++ import_lists.sh
@@ -3,11 +3,14 @@
 SUCCESS=0
 ERROR=1
 
-PIHOLE_CMD="/usr/local/bin/pihole"
-FTL_COMMAND="/usr/bin/pihole-FTL"
-SQL_EXEC_CMD="$FTL_COMMAND sqlite3"
-
 DB=/etc/pihole/gravity.db
+BACKUP_DB=/etc/pihole/gravity_pre_refresh_backup.db
+
+PIHOLE_CMD="/usr/local/bin/pihole"
+FTL_CMD="/usr/bin/pihole-FTL"
+SQL_EXEC_CMD="$FTL_CMD sqlite3 $DB"
+
+BL_FILE=list_of_blocklists.txt
 
 WE_FILE=whitelist_exact.txt
 WE_FLAGS='whitelist --noreload --quiet'
@@ -78,7 +81,111 @@ else
     exit $ERROR
 fi
 
-echo Using "$FTL_CMD $SQL_EXEC_CMD $DB" to access Gravity DB...
+
+# Check that we have a blocklist file at all, otherwise we dont want to make noise...
+
+if [ -f $BL_FILE -a -r $BL_FILE ]; then
+
+    echo Blocklist file $BL_FILE found!
+    echo Using "$SQL_EXEC_CMD" to access Gravity DB...
+
+    # Before doing anything serious, we make a full DB backup
+    echo Backing up DB $DB to $BACKUP_DB...
+
+$SQL_EXEC_CMD <<EOF
+.backup main $BACKUP_DB
+EOF
+
+    echo Importing...
+    
+$SQL_EXEC_CMD <<EOF
+DROP TABLE IF EXISTS tmp_adlist_import
+EOF
+
+$SQL_EXEC_CMD <<EOF
+CREATE TABLE tmp_adlist_import (address TEXT NOT NULL UNIQUE)
+EOF
+
+$SQL_EXEC_CMD <<EOF
+.schema tmp_adlist_import
+EOF
+
+    cat $BL_FILE | grep -v '#' | grep -v -e '^$' | sort | uniq | while read LINE
+    do
+
+$SQL_EXEC_CMD <<EOF
+INSERT INTO tmp_adlist_import (address) VALUES ('$LINE');
+EOF
+
+    done
+
+    echo Done inserting blocklist entries - now verifying...
+
+$SQL_EXEC_CMD <<EOF
+SELECT address FROM tmp_adlist_import ORDER BY address;
+SELECT COUNT(*) FROM tmp_adlist_import;
+EOF
+
+#
+# Now we will update the Gravity DB table adlist from our imported file, as follows:
+# - mark lists that were not imported as disabled
+# - mark lists that were disabled but re-imported as enabled
+# - insert new lists that were not present before
+# So because we do not delete any records from adlist, no group assignments will be lost!
+#
+
+    echo
+    echo Now processing imported lists...
+    echo
+    echo "1) Disabling obsolete lists..."
+
+$SQL_EXEC_CMD <<EOF
+.changes on
+UPDATE adlist
+   SET enabled = FALSE
+ WHERE enabled = TRUE
+   AND NOT EXISTS
+      (SELECT 1
+         FROM tmp_adlist_import
+        WHERE adlist.address = tmp_adlist_import.address);
+EOF
+
+    echo "2) Reenabling disabled lists that are in imported blocklist file..."
+
+$SQL_EXEC_CMD <<EOF
+.changes on
+UPDATE adlist
+   SET enabled = TRUE
+ WHERE enabled = FALSE
+   AND EXISTS
+      (SELECT 1
+         FROM tmp_adlist_import
+        WHERE adlist.address = tmp_adlist_import.address);
+EOF
+
+    echo "3) Importing new list entries..."
+
+$SQL_EXEC_CMD <<EOF
+.changes on
+INSERT INTO adlist (address, enabled)
+SELECT tmp_adlist_import.address, TRUE
+  FROM tmp_adlist_import
+ WHERE NOT EXISTS
+      (SELECT 1
+         FROM adlist
+        WHERE adlist.address = tmp_adlist_import.address);
+EOF
+
+    # For the time being, we do not drop our temporary import table now, so that
+    # it is possible to review its contents and eventually debug issues from the import
+
+    echo Done with processing the imported lists!
+
+
+else
+    echo No blocklist file $BL_FILE found, will not update adlist table in Gravity DB!
+fi
+
 
 echo Restarting DNS service to get rid of DB locks before updating lists...
 $PIHOLE_CMD $RELOAD_FLAGS
