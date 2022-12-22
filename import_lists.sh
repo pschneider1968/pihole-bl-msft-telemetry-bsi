85a5d54c06bd77d8f2354ea81bfaa9f310f884cf
Peter Schneider <36973164+pschneider1968@users.noreply.github.com>
2022-12-22 04:28:13 +0100

    Dev v0.2 (#4)
    * Added Notepad++ *.session file pattern to .gitignore
    
    * Added some URL shorteners to whitelist
    
    * Add git status
    
    * Prepare direct usage of Gravity DB to import list of domain block lists
    
    * Blocklist file list_of_blocklists.txt is now also imported into pihole Gravity.db via sqlite3
    
    * Simplify to one SQL statement
    
    * Make refresh_all.sh a bit more generic
    
    * Added versioning and parameter parsing; added a domain to whitelist_exact.txt
    
    * Finalize v0.2-RC1: all blocklists will now be imported automatically, depending on mode specified
    
    * Edit of list of Microsoft telemetry hosts, based on latest published information from BSI Bund Germany

--- import_lists.sh
+++ import_lists.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/bash
 
+MY_VERSION='v0.2'
+MY_YEAR='2022'
+BANNER="$(basename $0) $MY_VERSION (c) $MY_YEAR Peter Schneider, provided under MIT License"
+
 SUCCESS=0
 ERROR=1
 
-PIHOLE_CMD=/usr/local/bin/pihole
+DB='/etc/pihole/gravity.db'
+BACKUP_DB='/etc/pihole/gravity_pre_refresh_backup.db'
+
+PIHOLE_CMD='/usr/local/bin/pihole'
+FTL_CMD='/usr/bin/pihole-FTL'
+SQL_EXEC_CMD="$FTL_CMD sqlite3 $DB"
+
+BL_FILE=list_of_blocklists.txt
 
 WE_FILE=whitelist_exact.txt
 WE_FLAGS='whitelist --noreload --quiet'
@@ -22,6 +33,8 @@ NUKE_FLAGS='--nuke'
 UPDATE_DB_FLAGS='-g'
 RELOAD_FLAGS='restartdns reload-lists'
 
+
+# Functions
 process_file () {
 
 if [ -f $1 ]; then
@@ -34,7 +47,8 @@ if [ -f $1 ]; then
         $PIHOLE_CMD $2 $LINE
 
         # sleep to avoid DB locks due to commands in too fast succession
-        sleep 0.1
+        sleep 0.5
+        sync
     done
 
 else
@@ -43,6 +57,45 @@ fi
 
 }
 
+# Banner
+echo $BANNER
+
+# Mode of operation
+MODE=${1^^}
+
+if [ -z "$MODE" ]; then         # default mode
+    MODE='ADD'
+fi
+
+if [ "$MODE" = "HELP" -o "$MODE" = "--HELP" -o "$MODE" = "-H" -o "$MODE" = "-?" -o "$MODE" = "/?" ]; then
+    echo 
+    echo "Synopsis: import_lists.sh [MODE]"
+    echo
+    echo "This script will import the contents of the supplied file \"list_of_blocklists.txt\" into your Pi-Hole Gravity DB,"
+    echo "where MODE may be one of:"
+    echo
+    echo "   - HELP:    display this help info"
+    echo "   - ADD:     only add new lists, don't do anything to existing lists. This is the recommended mode of operation"
+    echo "              when you have other sources for your block lists, too, other than my repo."
+    echo "              It is also the default when no MODE is specified."
+    echo "   - MERGE:   add new lists, disable missing ones, re-enable disabled existing lists if they are in the import file."
+    echo "              This retains group assignments on existing list entries. This is the recommended mode of operation"
+    echo "              when my repo is the ONLY source of block lists for your Pi-Hole installation."
+    echo "   - DELETE:  add new lists, delete missing ones, re-enable disabled existing lists if they are in the import file."
+    echo "              Group assignments on deleted groups are of course lost, and they cannot just be re-enabled again,"
+    echo "              but will be newly imported when they happen to be in the next import file again."
+    echo "   - FULL:    fully replace all existing list entries in Gravity DB with the imported ones. Group assignments are thus lost."
+    echo "              That means that before inserting anything from the import file, everything is deleted in the Gravity DB."
+    echo 
+elif [ ! "$MODE" = "ADD" -a ! "$MODE" = "MERGE" -a ! "$MODE" = "DELETE" -a ! "$MODE" = "FULL" ]
+then
+    echo "ERROR: Unknown mode $MODE, please use the parameter \"HELP\" for information on script usage!"
+    exit $ERROR
+else
+    echo "Mode of import operation is $MODE"
+fi
+
+# check that all we need to handle can be found
 if [ ! -z $PIHOLE_CMD ]; then
     if [ -x $PIHOLE_CMD ]; then
         echo Found executable Pi-Hole command $PIHOLE_CMD!
@@ -54,6 +107,205 @@ else
     echo Pi-Hole command $PIHOME_CMD not found, aborting!
     exit $ERROR
 fi
+
+if [ ! -z $FTL_CMD ]; then
+    if [ -x $FTL_CMD ]; then
+        echo Found executable pihole-FTL command $FTL_CMD!
+    else
+        echo pihole-FTL command $FTL_CMD not found, aborting!
+        exit $ERROR
+    fi
+else
+    echo pihole-FTL command $FTL_CMD not found, aborting!
+    exit $ERROR
+fi
+
+if [ -f $DB ]; then
+    echo Found Gravity DB $DB!
+else
+    echo Gravity DB $DB not found, aborting!
+    exit $ERROR
+fi
+
+
+# Check that we have a blocklist file at all, otherwise we dont want to make noise...
+if [ -f $BL_FILE -a -r $BL_FILE ]; then
+
+    echo Found blocklist file $BL_FILE!
+    echo Using "$SQL_EXEC_CMD" to access Gravity DB...
+
+    # Before doing anything serious, we make a full DB backup
+    echo Backing up DB $DB to $BACKUP_DB...
+
+$SQL_EXEC_CMD <<EOF
+.backup main $BACKUP_DB
+EOF
+
+    echo Importing...
+    
+$SQL_EXEC_CMD <<EOF
+DROP TABLE IF EXISTS tmp_adlist_import;
+CREATE TABLE tmp_adlist_import (address TEXT NOT NULL UNIQUE);
+.schema tmp_adlist_import
+EOF
+
+    cat $BL_FILE | grep -v '#' | grep -v -e '^$' | sort | uniq | while read LINE
+    do
+
+$SQL_EXEC_CMD <<EOF
+INSERT INTO tmp_adlist_import (address) VALUES ('$LINE');
+EOF
+
+        # sleep a bit to avoid DB lock issues...
+        sleep 0.1
+
+    done
+
+    echo Done inserting blocklist entries - now verifying...
+    echo
+    echo --------------------------------------------------
+    echo Imported lists:
+    echo --------------------------------------------------
+    echo
+
+$SQL_EXEC_CMD <<EOF
+SELECT address FROM tmp_adlist_import ORDER BY address;
+.print
+.print Number of imported lists:
+.print --------------------------
+.print
+SELECT COUNT(*) FROM tmp_adlist_import;
+.print
+EOF
+
+#
+# Now we will update the Gravity DB table adlist from our imported file,
+# depending on our specified import mode:
+# - delete everything in FULL mode
+# - delete missing lists in DELETE mode
+# - mark lists that were not imported as disabled in MERGE mode
+# - mark lists that were disabled but re-imported as enabled in DELETE and MERGE mode
+# - insert new lists that were not present before in ALL modes
+# - in ADD mode, we never delete or disable lists
+# - in MERGE mode, we never delete lists
+# - in DELETE mode, we only delete lists not in the import file
+#
+
+    echo
+    echo Now processing imported lists with mode $MODE...
+    echo
+
+    if [ "$MODE" = "FULL" ]; then
+
+        echo "Deleting tables gravity and adlist..."
+
+$SQL_EXEC_CMD <<EOF
+.changes on
+.print gravity...
+DELETE FROM gravity;
+.print adlist...
+DELETE FROM adlist;
+EOF
+
+    fi  # FULL
+
+
+    if [ "$MODE" = "DELETE" ]; then
+
+        echo "Deleting missing lists from tables gravity and adlist..."
+
+$SQL_EXEC_CMD <<EOF
+.changes on
+.print gravity...
+DELETE FROM gravity
+ WHERE gravity.adlist_id IN
+      (SELECT adlist.id
+         FROM adlist
+        WHERE adlist.address NOT IN
+             (SELECT tmp_adlist_import.address
+                FROM tmp_adlist_import
+             )
+      );
+.print adlist...
+DELETE FROM adlist
+ WHERE NOT EXISTS
+      (SELECT 1
+         FROM tmp_adlist_import
+        WHERE adlist.address = tmp_adlist_import.address);
+EOF
+
+    echo "Reenabling disabled lists that are in imported blocklist file..."
+
+$SQL_EXEC_CMD <<EOF
+.changes on
+UPDATE adlist
+   SET enabled = TRUE
+ WHERE enabled = FALSE
+   AND EXISTS
+      (SELECT 1
+         FROM tmp_adlist_import
+        WHERE adlist.address = tmp_adlist_import.address);
+EOF
+
+    fi  # DELETE
+
+
+    if [ "$MODE" = "MERGE" ]; then
+
+        echo "Disabling obsolete lists..."
+
+$SQL_EXEC_CMD <<EOF
+.changes on
+UPDATE adlist
+   SET enabled = FALSE
+ WHERE enabled = TRUE
+   AND NOT EXISTS
+      (SELECT 1
+         FROM tmp_adlist_import
+        WHERE adlist.address = tmp_adlist_import.address);
+EOF
+
+    echo "Reenabling disabled lists that are in imported blocklist file..."
+
+$SQL_EXEC_CMD <<EOF
+.changes on
+UPDATE adlist
+   SET enabled = TRUE
+ WHERE enabled = FALSE
+   AND EXISTS
+      (SELECT 1
+         FROM tmp_adlist_import
+        WHERE adlist.address = tmp_adlist_import.address);
+EOF
+
+    fi  # MERGE
+
+
+    # Common operations for all modes
+    echo "Importing new list entries..."
+
+$SQL_EXEC_CMD <<EOF
+.changes on
+INSERT INTO adlist (address, enabled)
+SELECT tmp_adlist_import.address, TRUE
+  FROM tmp_adlist_import
+ WHERE NOT EXISTS
+      (SELECT 1
+         FROM adlist
+        WHERE adlist.address = tmp_adlist_import.address);
+EOF
+
+    # For the time being, we do not drop our temporary import table now, so that
+    # it is possible to review its contents and eventually debug issues from the import
+
+    echo
+    echo Done with processing the imported lists!
+
+
+else
+    echo No blocklist file $BL_FILE found, will not update adlist table in Gravity DB!
+fi
+
 
 echo Restarting DNS service to get rid of DB locks before updating lists...
 $PIHOLE_CMD $RELOAD_FLAGS
--- refresh_all.sh
+++ refresh_all.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 
-DIRECTORY="/etc/pihole/pihole-bl-msft-telemetry-bsi"
-REFRESH_CMD="git pull --no-rebase"
+# We assume the directory of this script is the cloned Git repo
+MY_DIRECTORY=$(dirname $0)
+
 LOG_FILE="/var/log/pihole/refresh_pihole_lists.log"
-REFRESH_SCRIPT="import_lists.sh"
+GIT_STATUS_CMD="git status"
+GIT_PULL_CMD="git pull --no-rebase"
+REFRESH_SCRIPT="import_lists.sh $1"
 
 touch $LOG_FILE
 
@@ -13,8 +16,14 @@ echo -------------------------------------------------- | tee -a $LOG_FILE | cat
 
 echo | tee -a $LOG_FILE | cat
 
-cd $DIRECTORY
-$REFRESH_CMD | tee -a $LOG_FILE | cat
+cd $MY_DIRECTORY
+$GIT_STATUS_CMD | tee -a $LOG_FILE | cat
+
+echo Pulling from remote repo... | tee -a $LOG_FILE | cat
+$GIT_PULL_CMD | tee -a $LOG_FILE | cat
+$GIT_STATUS_CMD | tee -a $LOG_FILE | cat
+
+echo
 
 $SHELL $REFRESH_SCRIPT | tee -a $LOG_FILE | cat
 
