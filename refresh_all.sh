56109936ad20961ab14151d4c44ee8984aa5bf73
pschneider1968 <36973164+pschneider1968@users.noreply.github.com>
2022-12-23 05:26:58 +0100

    Fixed problems when refresh_all.sh is executed in cron job and /bin/sh is a symlink to /usr/bin/dash

--- refresh_all.sh
+++ refresh_all.sh
@@ -7,28 +7,34 @@ LOG_FILE="/var/log/pihole/refresh_pihole_lists.log"
 GIT_STATUS_CMD="git status"
 GIT_PULL_CMD="git pull --no-rebase"
 REFRESH_SCRIPT="import_lists.sh $1"
+SHELL=/bin/bash
+
+if [[ ! ":$PATH:" == *":/usr/local/bin:"* ]]; then
+    PATH=/usr/local/bin:$PATH
+fi
 
 touch $LOG_FILE
 
-echo -------------------------------------------------- | tee -a $LOG_FILE | cat
-echo Refreshing lists at $(date)                        | tee -a $LOG_FILE | cat
-echo -------------------------------------------------- | tee -a $LOG_FILE | cat
-
-echo | tee -a $LOG_FILE | cat
+echo "--------------------------------------------------" 2>&1 | tee -a $LOG_FILE | cat
+echo "Refreshing lists at $(date)                       " 2>&1 | tee -a $LOG_FILE | cat
+echo "--------------------------------------------------" 2>&1 | tee -a $LOG_FILE | cat
+echo "" 2>&1 | tee -a $LOG_FILE | cat
+echo "PATH=$PATH" 2>&1 | tee -a $LOG_FILE | cat
+echo "" 2>&1 | tee -a $LOG_FILE | cat
 
 cd $MY_DIRECTORY
-$GIT_STATUS_CMD | tee -a $LOG_FILE | cat
+$GIT_STATUS_CMD 2>&1 | tee -a $LOG_FILE | cat
 
-echo Pulling from remote repo... | tee -a $LOG_FILE | cat
-$GIT_PULL_CMD | tee -a $LOG_FILE | cat
-$GIT_STATUS_CMD | tee -a $LOG_FILE | cat
+echo "Pulling from remote repo..." 2>&1 | tee -a $LOG_FILE | cat
+$GIT_PULL_CMD 2>&1 | tee -a $LOG_FILE | cat
+$GIT_STATUS_CMD 2>&1 | tee -a $LOG_FILE | cat
 
-echo
+echo "" 2>&1 | tee -a $LOG_FILE | cat
 
-$SHELL $REFRESH_SCRIPT | tee -a $LOG_FILE | cat
+$SHELL $REFRESH_SCRIPT 2>&1 | tee -a $LOG_FILE | cat
 
-echo ------------------------------------------------- | tee -a $LOG_FILE | cat
-echo Done refreshing lists at $(date)                  | tee -a $LOG_FILE | cat
-echo ------------------------------------------------- | tee -a $LOG_FILE | cat
+echo "-------------------------------------------------" 2>&1 | tee -a $LOG_FILE | cat
+echo "Done refreshing lists at $(date)                 " 2>&1 | tee -a $LOG_FILE | cat
+echo "-------------------------------------------------" 2>&1 | tee -a $LOG_FILE | cat
 
-echo | tee -a $LOG_FILE | cat
+echo "" 2>&1 | tee -a $LOG_FILE | cat
